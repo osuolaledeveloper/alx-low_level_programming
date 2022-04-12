bb65666ad554564a2a05c7624d683cdfbb47f31a
osuolale hezekiah <osuolalehezekiah@gmail.com>
2022-04-12 18:22:37 +0000

    alc

---
+++ 0-putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+ int main(){
+	 char c[] = "_putchar" ;
+	 int i;
+	 for (i = 0; i < 8; i ++){
+		 _putchar(c[i]);
+	 }
+	 return 0;
+ }
---
+++ README.md
@@ -0,0 +1,1 @@
+0: printing _putchar out 
